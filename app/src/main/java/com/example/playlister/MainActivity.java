4460139c257e62c33e0ba96fd10e5c1c995996c4
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2022-05-31 09:11:21 +0200

    Rody --> Got the login request to the API to work.

--- app/src/main/java/com/example/playlister/MainActivity.java
+++ app/src/main/java/com/example/playlister/MainActivity.java
@@ -3,14 +3,36 @@ package com.example.playlister;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView signUpLink;
+    Button loginButton;
+    EditText loginFormUserName;
+    EditText loginFormPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
                 openSignUpActivity();
             }
         });
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handlelogin();
+            }
+        });
+        loginFormUserName = (EditText) findViewById(R.id.loginFormUserName);
+        loginFormPassword = (EditText) findViewById(R.id.loginFormPassword);
     }
 
     public void openSignUpActivity() {
@@ -30,10 +61,49 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void login() {
-        new AlertDialog.Builder(this)
-                .setTitle("Login")
-                .setMessage("Username --> " + "" + "")
-                .show();
+    public void handlelogin() {
+        String username = loginFormUserName.getText().toString();
+        String password = loginFormPassword.getText().toString();
+        String apiResponse = "No response yet.";
+        String requestURL = Utils.getString(this, R.string.authURL);
+        JSONObject formData = new JSONObject();
+        try {
+            formData.put("username", username);
+            formData.put("password", password);
+            RequestQueue queue = Volley.newRequestQueue(this);
+            Context thisContext = this;
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                    requestURL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            new AlertDialog.Builder(thisContext)
+                                    .setTitle("API AUTH TOKEN")
+                                    .setMessage(response.toString())
+                                    .show();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    new AlertDialog.Builder(thisContext)
+                            .setTitle("ERROR")
+                            .setMessage(error.toString())
+                            .show();
+
+                }
+            }) {
+                @Override
+                public byte[] getBody() {
+                    Map<String, String> body = new HashMap<String, String>();
+                    body.put("username", username);
+                    body.put("password", password);
+                    String bodyString = "username=" + username + "&password=" + password;
+                    return bodyString.getBytes(StandardCharsets.UTF_8);
+                }
+            };
+            queue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlister/Utils.java
@@ -0,0 +1,10 @@
+package com.example.playlister;
+
+import android.content.Context;
+
+public class Utils {
+
+    public static String getString(Context context, int id) {
+        return context.getResources().getString(id);
+    }
+}
