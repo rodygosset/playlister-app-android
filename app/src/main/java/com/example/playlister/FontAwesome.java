fd33a63edca9967614c5d510f748bfb9fc890c2d
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2022-06-10 13:43:37 +0200

    Rody --> Got the login logic to work, and started integrating the home layout.

---
+++ app/src/main/java/com/example/playlister/FontAwesome.java
@@ -0,0 +1,36 @@
+package com.example.playlister;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+@SuppressLint("AppCompatCustomView")
+public class FontAwesome extends TextView {
+
+
+    public FontAwesome(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public FontAwesome(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public FontAwesome(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+
+        //Font name should not contain "/".
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(),
+                "font-solid-900.ttf");
+        setTypeface(tf);
+    }
+
+}
